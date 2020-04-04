b24b1aedf942ad1d0b3d77e961db29cd8d0454a6
Simeng He <simengschool@gmail.com>
2020-04-04 16:38:14 -0400

    gin and juice

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	const PORT = ":5000"
+	r.Run(PORT)
+}
